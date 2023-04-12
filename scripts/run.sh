0ebe1e210ad57d7253a13e26a1836a46d2203844
The.soul.Of.Pixels <81808405+sofpixaol@users.noreply.github.com>
2023-04-12 23:38:02 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ xrdb merge ~/.Xresources
 xbacklight -set 10 &
 feh --randomize --bg-fill ~/Pictures/wallpaper/*.png &
 xset r rate 200 50 &
-picom --experimental-backends --config ~/.config/chadwm/scripts/config/picom.conf >> /dev/null 2>&1 &
+picom --experimental-backends --config ~/.config/chadwm/scripts/picom.conf >> /dev/null 2>&1 &
 
 ~/.config/chadwm/scripts/bar.sh &
 while type dwm >/dev/null; do dwm && continue || break; done
