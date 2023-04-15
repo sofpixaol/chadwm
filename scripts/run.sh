15d59e4cc684a6596c27d3ec9cf69072f19163c5
The.soul.Of.Pixels <81808405+sofpixaol@users.noreply.github.com>
2023-04-15 18:43:08 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,7 @@ xset r rate 200 50 &
 picom --experimental-backends --config ~/.config/chadwm/scripts/picom.conf >> /dev/null 2>&1 &
 dunst -conf ~/scripts/config/dunst.conf &
 flameshot &
+eww open eww &
 
 ~/.config/chadwm/scripts/bar.sh &
 while type dwm >/dev/null; do dwm && continue || break; done
